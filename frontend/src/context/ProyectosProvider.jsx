62d4be22f07a7a64bfd036faae7f645115e8a24e
abousiyd <abdou.bousiyd@gmail.com>
2022-03-23 17:46:04 +0100

    creat hook para el context de proyectos

--- frontend/src/context/ProyectosProvider.jsx
+++ frontend/src/context/ProyectosProvider.jsx
@@ -5,10 +5,13 @@ import axios from '../../axios'
 const ProyectosContext = createContext()
 
 const ProyectosProvider = ({children}) => {
+
+    const [proyectos, setProyectos] = useState([])
+
     return (
         <ProyectosContext.Provider
             value={{
-                
+                proyectos
             }}
         >{children}
         </ProyectosContext.Provider> 
--- frontend/src/paginas/Proyectos.jsx
+++ frontend/src/paginas/Proyectos.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import useProyectos from "../hooks/useProyectos"
+
 
 const Proyectos = () => {
+
+  const {proyectos} = useProyectos()
+  console.log(proyectos, 33322111)
+
   return (
     <>
       <h1 className='text-4xl font-black'>Proyectos</h1>
